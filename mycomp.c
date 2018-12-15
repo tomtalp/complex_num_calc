5f5a36161073a3d48ea29b6ac84dc598ad75cbaf
Tom T <tomtalp93@gmail.com>
2018-12-15 17:24:29 +0200

    WIP

--- mycomp.c
+++ mycomp.c
@@ -1,7 +1,11 @@
+#include <math.h> 
 #include <stdio.h>
+#include "foo.h"
+
+
 typedef struct Complex {
-    float realNum;
-    float imaginaryNum;
+    float realVal;
+    float imaginaryVal;
 } Complex;
 
 typedef struct Variables {
@@ -44,8 +48,8 @@ Variables init_variables() {
 }
 
 void init_complex(Complex * c) {
-    c->realNum = 0;
-    c->imaginaryNum = 0;
+    c->realVal = 0;
+    c->imaginaryVal = 0;
 }
 
 Complex * access_variable(Variables * v, char varName) {
@@ -61,51 +65,51 @@ Complex * access_variable(Variables * v, char varName) {
 }
 
 void read_comp(Complex *c, float realVal, float imaginaryVal) {
-    c->realNum = realVal;
-    c->imaginaryNum = imaginaryVal;
+    c->realVal = realVal;
+    c->imaginaryVal = imaginaryVal;
 }
 
 void print_comp(Complex *c) {
-    printf("%0.2f", c->realNum);
-    if(c->imaginaryNum >= 0) {
-        printf(" + (%0.2f)i", c->imaginaryNum);
+    printf("%0.2f", c->realVal);
+    if(c->imaginaryVal >= 0) {
+        printf(" + (%0.2f)i", c->imaginaryVal);
     } else {
-        printf(" - (%0.2f)i ", c->imaginaryNum*-1);
+        printf(" - (%0.2f)i ", c->imaginaryVal*-1);
     }
     printf("\n");
 }
 
 void add_comp(Complex *c1, Complex *c2) {
     Complex result;
-    float realSum = c1->realNum + c2->realNum;
-    float imaginarySum = c1->imaginaryNum + c2->imaginaryNum;
-    read_comp(&result, realSum, imaginarySum);
+    float realVal = c1->realVal + c2->realVal;
+    float imaginaryVal = c1->imaginaryVal + c2->imaginaryVal;
+    read_comp(&result, realVal, imaginaryVal);
     print_comp(&result);
 }
 
 void sub_comp(Complex *c1, Complex *c2) {
     Complex result;
-    float realSum = c1->realNum - c2->realNum;
-    float imaginarySum = c1->imaginaryNum - c2->imaginaryNum;
-    read_comp(&result, realSum, imaginarySum);
+    float realVal = c1->realVal - c2->realVal;
+    float imaginaryVal = c1->imaginaryVal - c2->imaginaryVal;
+    read_comp(&result, realVal, imaginaryVal);
     print_comp(&result);
 }
 
 void mult_comp_real(Complex *c1, float r1) {
     Complex result;
 
-    float realVal = r1 * c1->realNum;
-    float imaginaryVal = r1 * c1->imaginaryNum;
+    float realVal = r1 * c1->realVal;
+    float imaginaryVal = r1 * c1->imaginaryVal;
 
     read_comp(&result, realVal, imaginaryVal);
     print_comp(&result);
 }
 
-void mult_comp_imaginary(Complex *c1, float imaginaryMultiplier) {
+void mult_comp_img(Complex *c1, float imaginaryMultiplier) {
     Complex result;
 
-    float realVal = (-1) * imaginaryMultiplier * c1->imaginaryNum;
-    float imaginaryVal = imaginaryMultiplier * c1->realNum;
+    float realVal = (-1) * imaginaryMultiplier * c1->imaginaryVal;
+    float imaginaryVal = imaginaryMultiplier * c1->realVal;
 
     read_comp(&result, realVal, imaginaryVal);
     print_comp(&result);
@@ -114,53 +118,62 @@ void mult_comp_imaginary(Complex *c1, float imaginaryMultiplier) {
 void mult_comp_comp(Complex *c1, Complex *c2) {
     Complex result;
 
-    float realVal = (c1->realNum * c2->realNum) - (c1->imaginaryNum * c2->imaginaryNum);
-    float imaginaryVal = (c1->realNum * c2->imaginaryNum) + (c1->imaginaryNum * c2->realNum);
+    float realVal = (c1->realVal * c2->realVal) - (c1->imaginaryVal * c2->imaginaryVal);
+    float imaginaryVal = (c1->realVal * c2->imaginaryVal) + (c1->imaginaryVal * c2->realVal);
 
     read_comp(&result, realVal, imaginaryVal);
     print_comp(&result);
 }
 
-int main() {
-    printf("Hello\n");
-
-    Variables v1;
-
-    v1 = init_variables();
-
-    // Set A - 
-    Complex * A = access_variable(&v1, 'A');
-    read_comp(A, 3.1, 5.3);
-
-    // Set B - 
-    Complex * B = access_variable(&v1, 'B');
-    read_comp(B, 18.2, 20);
-
-    printf("A = ");
-    print_comp(A);
-    printf("B = ");
-    print_comp(B);
-    printf("\n\n");
-
-    printf("A+B = ");
-    add_comp(A, B);
-    printf("\n");
-
-    printf("A-B = ");
-    sub_comp(A, B);
-    printf("\n");
-
-    printf("A * 3 = ");
-    mult_comp_real(A, 3);
-    printf("\n");
-
-    printf("A * 5i = ");
-    mult_comp_imaginary(A, 5);
-    printf("\n");
-
-    printf("A * B = ");
-    mult_comp_comp(A, B);
-    printf("\n");
-    
-    return 0;
+void abs_comp(Complex *c1) {
+    float absVal = sqrt(pow(c1->realVal, 2) + pow(c1->imaginaryVal, 2));
+    printf("%0.2f", absVal);
 }
+
+// int main() {
+//     printf("Hello\n");
+
+//     Variables v1;
+
+//     v1 = init_variables();
+
+//     // Set A - 
+//     Complex * A = access_variable(&v1, 'A');
+//     read_comp(A, 3.1, 5.3);
+
+//     // Set B - 
+//     Complex * B = access_variable(&v1, 'B');
+//     read_comp(B, 18.2, 20);
+
+//     printf("A = ");
+//     print_comp(A);
+//     printf("B = ");
+//     print_comp(B);
+//     printf("\n\n");
+
+//     printf("A+B = ");
+//     add_comp(A, B);
+//     printf("\n");
+
+//     printf("A-B = ");
+//     sub_comp(A, B);
+//     printf("\n");
+
+//     printf("A * 3 = ");
+//     mult_comp_real(A, 3);
+//     printf("\n");
+
+//     printf("A * 5i = ");
+//     mult_comp_img(A, 5);
+//     printf("\n");
+
+//     printf("A * B = ");
+//     mult_comp_comp(A, B);
+//     printf("\n");
+
+//     printf("|A| = ");
+//     abs_comp(A);
+//     printf("\n");
+    
+//     return 0;
+// }
